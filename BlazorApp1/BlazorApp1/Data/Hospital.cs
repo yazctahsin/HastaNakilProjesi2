575b4249d8651b60135e164daa533d1310e5abdf
yazctahsin <117100542+yazctahsin@users.noreply.github.com>
2025-08-31 17:05:25 +0300

    Eklendi
    Projenin baz dosyaları eklendi

---
+++ BlazorApp1/BlazorApp1/Data/Hospital.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp1.Data
+{
+    public class Hospital { 
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
